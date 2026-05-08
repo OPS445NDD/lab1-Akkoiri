d8cdd630fb863fa43b2795d62b3ce17e5fb3ec21
Ambika Koiri <akkoiri@myseneca.ca>
2026-05-08 10:43:50 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program performs a math calculation.'''
+
+x = 10
+y = 2
+z = 5
+
+result = x + y * z
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
